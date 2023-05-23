d635d4214e442b2e101a5a19593bb6f4b471faaa
Emanoel <emanoelsilvaif@gmail.com>
2023-05-22 21:51:00 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "react-native";
+import Home from "./src/screens/Home";
+
+function App() {
+  return (
+    <>
+      <StatusBar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
